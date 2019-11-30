a9f59931f0d4ee5a5e691b2a6cfedab3a3dc6995
Daniel Kluzner <danielkluzner@cs.ucsb.edu>
2019-11-29 17:05:58 -0800

    dk - modified getJSON method of location search to return real results

--- src/main/java/earthquakes/services/LocationQueryService.java
+++ src/main/java/earthquakes/services/LocationQueryService.java
@@ -24,23 +24,20 @@ public class LocationQueryService {
     private Logger logger = LoggerFactory.getLogger(LocationQueryService.class);
 
     public String getJSON(String location) {
-	String fakeJson = "{ \"key\": \"value\" }";
+	/*String fakeJson = "{ \"key\": \"value\" }";
 	String json = fakeJson;
 	logger.info("json=" + json);
-	return json;
-        /*RestTemplate restTemplate = new RestTemplate();
-
+	return json;*/
+        RestTemplate restTemplate = new RestTemplate();
+	
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
       
         HttpEntity<String> entity = new HttpEntity<>("body", headers);
 
-        String uri = "https://earthquake.usgs.gov/fdsnws/event/1/query";
-        double ucsbLat = 34.4140;
-        double ucsbLong = -119.8489;
-        String params = String.format("?format=geojson&minmagnitude=%d&maxradiuskm=%d&latitude=%f&longitude=%f",
-           minmag,distance,ucsbLat,ucsbLong);
+        String uri = "https://nominatim.openstreetmap.org/search/";
+	String params = String.format("%s?format=json", location);
 
         String url = uri + params;
         logger.info("url=" + url);
@@ -54,8 +51,8 @@ public class LocationQueryService {
         } catch (HttpClientErrorException e) {
             retVal = "{\"error\": \"401: Unauthorized\"}";
         }
-        logger.info("from EarthquakeQueryService.getJSON: " + retVal);
-        return retVal;*/
+        logger.info("from LocationQueryService.getJSON: " + retVal);
+        return retVal;
     }
 
 }
