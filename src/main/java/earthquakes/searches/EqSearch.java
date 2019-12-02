2d66ad24dce38c9343f7c76ce87063ffe2d83dd0
Daniel Kluzner <danielkluzner@cs.ucsb.edu>
2019-12-01 20:30:09 -0800

    dk - added lat, lon, and location fields to bean and action forms

--- src/main/java/earthquakes/searches/EqSearch.java
+++ src/main/java/earthquakes/searches/EqSearch.java
@@ -4,10 +4,16 @@ public class EqSearch {
 
     private int distance;
     private int minmag;
+    private double lat;
+    private double lon;
+    private String location;
 
     public EqSearch(){
 	distance = 0;
 	minmag = 0;
+	lat = 0.0;
+	lon = 0.0;
+	location = "";
     }
 
     public int getDistance(){
@@ -18,12 +24,36 @@ public class EqSearch {
 	return minmag;
     }
 
+    public double getLat(){
+        return lat;
+    }
+
+    public double getLon(){
+        return lon;
+    }
+
+    public String getLocation(){
+	return location;
+    }
+    
     public void setDistance(int distance){
 	this.distance = distance;
     }
 
     public void setMinmag(int minmag){
 	this.minmag = minmag;
+    }
+
+    public void setLat(double lat){
+	this.lat = lat;
+    }
+
+    public void setLon(double lon){
+	this.lon = lon;
+    }
+
+    public void setLocation(String location){
+	this.location = location;
     }
     
 }
